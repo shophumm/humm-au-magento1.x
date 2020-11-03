a95ccc78e3af4b72584d48a7120fee6c6f2ae847
Roger Bi <roger.bi@flexigroup.com.au>
2020-11-03 16:22:18 +1100

    update for new Version

--- app/code/community/Humm/Payments/Block/Info/HummPayments.php
+++ app/code/community/Humm/Payments/Block/Info/HummPayments.php
@@ -3,15 +3,18 @@
 /**
  * Class Humm_HummPayments_Info_Form_HummPayments
  * @Description Code behind for the custom Humm payment info block.
- * @Remarks Invokes the view template in a distant folder structure: ~/app/design/frontend/base/default/template/HummPayments/info.phtml
  *
  */
 class Humm_Payments_Block_Info_HummPayments extends Mage_Payment_Block_Info
 {
 
+
+    protected $_template = 'humm/payment/method/info/default.phtml';
+
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('humm/payments/info.phtml');
+        $this->setTemplate($this->_template);
     }
+
 }
